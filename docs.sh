3e33a1febf745e8e84737db289fe530c4ae84cc6
xenu256 <talaikis.tadas@gmail.com>
2018-06-25 12:52:57 +0300

    update build

--- docs.sh
+++ docs.sh
@@ -5,4 +5,4 @@ wget http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.3.1/swagge
 java -jar swagger-codegen-cli.jar generate -i http://localhost:3000/ -l ruby -o /home/ubuntu/blueblood/ruby/
 java -jar swagger-codegen-cli.jar generate -i http://localhost:3000/ -l go -o /home/ubuntu/blueblood/go/
 java -jar swagger-codegen-cli.jar generate -i http://localhost:3000/ -l python -o /home/ubuntu/blueblood/python/
-java -jar swagger-codegen-cli.jar generate -i http://localhost:3000/ -l typescript -o /home/ubuntu/blueblood/typescript/
+java -jar swagger-codegen-cli.jar generate -i http://localhost:3000/ -l javascript -o /home/ubuntu/blueblood/javascript/
